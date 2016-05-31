98b59f6d26689550712d71c51cae3e3ba4fcee2d
Lili <insa.lilitong@gmail.com>
2016-05-31 16:52:37 +0200

    update db

--- app.js
+++ app.js
@@ -47,15 +47,15 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
 
 //http server
 var httpserver = http.createServer(app);
